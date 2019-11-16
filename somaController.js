4bcd7355bd71aa3daf6bf64a01affaac77be7c2b
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2019-11-16 17:27:06 -0300

    meus gastos

--- somaController.js
+++ somaController.js
@@ -1,5 +1,13 @@
 angular.module('aplicacao').controller('somaController', function($scope) {
     $scope.guardar = []
 
-    
+    $scope.listar = function($scope) {
+        $scope.guardar.push($scope.valor)
+    }
+
+    $scope.usuarios = []
+
+    $scope.submeterLogin = function() {
+        $scope.usuarios.push($scope.x)
+    }
 })
